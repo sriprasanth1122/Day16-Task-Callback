87fed1090ce060c53fadb4561b6ddeb3e02d2fd7
Sri Prasanth <sriprasanth1122@gmail.com>
2023-06-21 13:55:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let i = 10;
+setTimeout(() => {
+    document.getElementById("display").innerHTML = i;
+    i--;
+    setTimeout(() => {
+        document.getElementById("display").innerHTML = i;
+        i--;
+        setTimeout(() => {
+            document.getElementById("display").innerHTML = i;
+            i--;
+            setTimeout(() => {
+                document.getElementById("display").innerHTML = i;
+                i--;
+                setTimeout(() => {
+                    document.getElementById("display").innerHTML = i;
+                    i--;
+                    setTimeout(() => {
+                        document.getElementById("display").innerHTML = i;
+                        i--;
+                        setTimeout(() => {
+                            document.getElementById("display").innerHTML = i;
+                            i--;
+                            setTimeout(() => {
+                                document.getElementById("display").innerHTML = i;
+                                i--;
+                                setTimeout(() => {
+                                    document.getElementById("display").innerHTML = i;
+                                    i--;
+                                    setTimeout(() => {
+                                        document.getElementById("display").innerHTML = i;
+                                        i--;
+                                        setTimeout(() => {
+                                            document.getElementById("display").innerHTML = "Happy Independence Day";
+                                            i--;
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
